ebad2337eb425e686b3a37ebb63f7d71c4a1cb0f
Sean Stapleton <seanstapleton@Seans-MacBook-Pro.local>
2016-07-10 15:41:07 -0500

    Calendar support added. Contact Form added. Layout adjusted for modular blocks. New images added.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var layoutData = { 
+      title: 'The Harp and Fiddle',
+      links: ["menu", "events", "contact", "parties", "gallery"],
+      homepageslides: ["/images/wooden.jpg","",""]
+  }
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-      title: 'The Harp and Fiddle',
-      links: ["Menu", "About", "Contact", "Parties", "Gallery"],
-      homepageslides: ["/images/wooden.jpg","",""]
-  });
+  res.render('index', layoutData);
+});
+
+/* GET about page. */
+router.get('/events', function(req, res, next) {
+  res.render('events', layoutData);
+});
+
+/* GET about page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', layoutData);
 });
 
 module.exports = router;
