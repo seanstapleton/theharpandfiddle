cf587b394c53534854ee6008b5b32f9b37eb732c
Sean Stapleton <seanstapleton100@gmail.com>
2016-08-29 16:22:30 -0500

    Hotfix for dropbox token

--- routes/index.js
+++ routes/index.js
@@ -199,7 +199,7 @@ module.exports = function(db, passport) {
     /* GET gallery page. */
     router.get('/gallery', function(req, res, next) {
 
-      var dbx = new Dropbox({ accessToken: 'ICSw9A7Vq4UAAAAAAAAJsMghP3GCSWRtV0NyyvHJVk0Mu0Wb2sdJBsN45BtKMdMJ' });
+      var dbx = new Dropbox({ accessToken: 'ICSw9A7Vq4UAAAAAAAAJ0s96q4fGsuFcwIoKIEvtCordgHLPuqLG9pASyArIi6-2' });
 
       var promises = [];
       var entries = [];
