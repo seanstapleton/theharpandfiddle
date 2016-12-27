dfce58b138c84372e3344de8881e5845bf326a89
Sean Stapleton <seanstapleton100@gmail.com>
2016-12-27 10:23:51 -0600

    contact page checkpoint

---
+++ public/js/controllers/contact.js
@@ -0,0 +1,27 @@
+(function() {
+  var app = angular.module('theharpandfiddle');
+  app.controller('ContactController', ['$scope','$http', function($scope, $http) {
+    $scope.submit = function() {
+      var name = $('#name').val(),
+          email = $('#email').val(),
+          phone = $('#phnum').val(),
+          subject = $('#subject').val(),
+          message = $('#message').val();
+
+          $('#name').val(validator.escape(name));
+          $('#email').val(validator.escape(email));
+          $('#phnum').val(validator.escape(phone));
+          $('#subject').val(validator.escape(subject));
+          $('#message').val(validator.escape(message));
+
+      $http.post('/backendServices/contact', $scope.formData)
+        .then(function(res) {
+          if (res.data.success) {
+            $scope.message = "Success! Thank you for your message.";
+          } else {
+            $scope.message = "Whoops! Unfortunately, this site may be broken. If you still wish to send your messsage, please contact declan@theharpandfiddle.com";
+          }
+        });
+    }
+  }]);
+}());
