33a23050855e158f32b58e3c42366f8086fb849e
Sean Stapleton <seanstapleton100@gmail.com>
2017-03-22 14:41:59 -0500

    Added animations and embedded beer menu

--- routes/backendServices.js
+++ routes/backendServices.js
@@ -9,165 +9,30 @@ module.exports = function(db, passport) {
     var eventsSchema    = require('../models/events.js');
     var specialsSchema  = require('../models/specials.js');
     var flash           = require('connect-flash');
-    var http            = require('http');
+    var https           = require('https');
     var Dropbox         = require('dropbox');
     var validator       = require('validator');
-    var hours;
 
-    hoursSchema.find({}, {'_id': false, 'order': false}, function(err, returnHours) {
-        hours = returnHours;
-    });
+    router.post('/insta', function(req, response, next) {
 
-    router.post('/eventUpload', function(req, res) {
-      var event = new eventsSchema({
-          title: req.body.title,
-          start: req.body.start,
-          end: req.body.end,
-          description: req.body.desc,
-          allDay: false,
-          url: req.body.url
-      });
-      event.save(function(err, ev) {
-        console.log(err);
-      });
-    });
+      const https = require('https');
 
-    router.get('/getHours', function(req, res, next) {
-      res.send(hours);
-    });
+      https.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + process.env.igaccess, (res) => {
 
-    /* GET home page. */
-    router.get('/loadWelcome', function(req, res, next) {
-        var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-        var d1 = new Date();
-        var d = new Date(d1.getTime() - 18000000);
-        var str = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2);
-        specialsSchema.find({dotw: days[d.getDay()]}, {'_id': false}, function(err, specials) {
-            eventsSchema.find({}, {'_id': false}, function(err, events) {
-
-              var dbx = new Dropbox({ accessToken: process.env.dropbox_token });
-
-              var promises = [];
-              var entries = [];
-
-              var push = function(path) {
-                return new Promise((resolve, reject) => {
-                  dbx.sharingCreateSharedLink({path: path})
-                    .then(function(response) {
-                      resolve(response.url.slice(0,-4) + "raw=1");
-                    })
-                });
-              }
-
-              dbx.filesListFolder({path: '/HF Photo Stream'})
-                .then(function(response) {
-                  for (var i = 0; i < response.entries.length; i++) {
-                    promises.push(push(response.entries[i].path_display));
-                  }
-                  Promise.all(promises).then(values => {
-                    res.send({
-                        entries: values,
-                        specials: specials,
-                        events: events
-                    });
-                  });
-                })
-                .catch(function(error) {
-                  console.log(error);
-                });
-            });
-        });
-    });
-
-    /* GET dinner menu page. */
-    router.get('/getMenu', function(req, res, next) {
-        menuSchema.findOne({"type": new RegExp('^' + req.query.menutype, "i")}, {'_id': false}, {sort: {"start": -1}}, function(err, menu) {
-            console.log(req.query.menutype);
-            console.log(menu, err);
-            res.send(menu);
-        });
-    });
-
-    /* GET dinner menu page. */
-    router.get('/getEvents', function(req, res, next) {
-        eventsSchema.find({}, {'_id': false}, {sort: {"start": -1}}, function(err, events) {
-            if (events) {
-              // var rel = [];
-              // var date = new Date();
-              // for (var i = 0; i < events.length; i++) {
-              //   var d = new Date(events[i].end);
-              //   if (d > new Date()) rel.push(events[i]);
-              // }
-              res.send(events);
-            } else {
-              res.end();
+        res.setEncoding('utf8');
+          let rawData = '';
+          res.on('data', (chunk) => rawData += chunk);
+          res.on('end', () => {
+            try {
+              let parsedData = JSON.parse(rawData);
+              response.send(parsedData);
+            } catch (e) {
+              console.log(e.message);
             }
-        });
-    });
-
-    /* POST contact page. */
-    router.post('/contact', function(req, res) {
-        if (!req.body.name) {res.send({success: false}); return false;};
-        var smtpTransporter = nodemailer.createTransport({
-          service: 'Mailgun',
-          auth: {
-            user: 'postmaster@sandbox8942345cff734588a349b18a65da4253.mailgun.org',
-            pass: 'e8ce8cf8eb9112b55cfd3f6bd09a8882'
-          }
-        });
-        var message = {
-          from: 'theharpandfiddle.com',
-          to: 'fiddlersonmain@gmail.com',
-          subject: 'Contact Form Submission: ' + req.body.subject,
-          text: "Name: " + req.body.name + "\n" + "Phone Number: " + req.body.phnum + "\n" + "Email: " + req.body.email + "\nMessage: " + req.body.message
-        };
-
-        smtpTransporter.sendMail(message, function(err, info) {
-           if (err) {
-               console.log(err);
-               res.send({
-                  success: false
-                });
-           } else {
-                console.log(req.body);
-                res.send({
-                  success: true
-                });
-           }
-        });
-    });
-
-    /* GET gallery page. */
-    router.get('/gallery', function(req, res, next) {
-
-      var dbx = new Dropbox({ accessToken: process.env.dropbox_token });
-
-      var promises = [];
-      var entries = [];
-
-      var push = function(path) {
-        return new Promise((resolve, reject) => {
-          dbx.sharingCreateSharedLink({path: path})
-            .then(function(response) {
-              resolve(response.url.slice(0,-4) + "raw=1");
-            })
-        });
-      }
-
-      dbx.filesListFolder({path: '/HF Photo Stream'})
-        .then(function(response) {
-          for (var i = 0; i < response.entries.length; i++) {
-            promises.push(push(response.entries[i].path_display));
-          }
-          Promise.all(promises).then(values => {
-            console.log("Values: ", values);
-            res.send(values);
           });
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-
+      }).on('error', (e) => {
+        console.error(e);
+      });
     });
 
     // /* GET logout page */
