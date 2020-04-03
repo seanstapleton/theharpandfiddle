39a1421c6f07707d757b498b2792fd878516b066
Sean Stapleton <seanstapleton100@gmail.com>
2020-04-03 13:34:07 -0400

    updated to mark as temporarily closed

--- public/js/index.js
+++ public/js/index.js
@@ -59,7 +59,7 @@ window.moment = Moment;
       isMobile = !window.matchMedia('(min-width: 960px)').matches;
     });
 
-    $(document).on('click', '.gov-order', function(evt)  {
+    $(document).on('click', '.temp-closed', function(evt)  {
       evt.preventDefault();
       $('#overlay').toggleClass('show');
       $('#gameWatchDeals').toggleClass('show');
